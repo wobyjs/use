f395a783058d02dd682cffdeb88ee06189efe346
wongchichong <wongchichong@gmail.com>
2023-06-02 09:14:29 +0800

    fix bugs

--- demo/ReactCounter.d.ts
+++
@@ -1,9 +0,0 @@
-export declare const ReductionCounter: ({ initValue, ...props }?: {
-    initValue?: number;
-}) => JSX.Element;
-export declare const ReactCounter: ({ initValue }?: {
-    initValue?: number;
-}) => JSX.Element;
-export declare const ReactAutoCounter: ({ initValue }?: {
-    initValue?: number;
-}) => JSX.Element;
--- demo/VobyInReact.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const VobyInReact: () => JSX.Element;
